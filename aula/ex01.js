0590f3ddbdd82dea7a6140e695bf3a5975778452
Flavio Silva <fal_silva@hotmail.com>
2023-06-16 14:42:45 -0300

    Pastas com aulas

---
+++ aula/ex01.js
@@ -0,0 +1,10 @@
+console.log('============ Exercicio 01 ==================')
+let a = 'vermelho'
+let b = 'azul'
+console.log(`Primeira cor da letra a: ${a}`)
+console.log(`Primeira cor da letra b: ${b}`)
+let c = a
+a = b
+b = c
+console.log(`Segunda cor da letra a: ${a}`)
+console.log(`Segunda cor da letra b: ${b}`)
