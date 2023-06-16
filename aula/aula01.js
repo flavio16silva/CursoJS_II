4064cf58d5b021bc1c92040dc15fb021408c832f
Flavio Silva <fal_silva@hotmail.com>
2023-06-16 14:09:22 -0300

    Laços

--- aula/aula01.js
+++ aula/aula01.js
@@ -254,14 +254,30 @@ console.log('============ LAÇO, LOOP, FOR ==================')
 //for
 for(let i = 5; i >= 1; i--) {
   if ( i % 2 !== 0) {
-    console.log(`o valor de i, com resto diferente de zero é ${i}`)
+    console.log(`o valor de i, com resto diferente de zero no laço FOR é ${i}`)
   } else {
-    console.log(`o valor de i, com resto igual de zero é ${i}`)
+    console.log(`o valor de i, com resto igual de zero no laço FOR é ${i}`)
   }  
 }
 
 console.log('============ LAÇO, LOOP, WHILE ==================')
+//while
+let i = 0           //variavel fora do laço
+while (i < 5) {     //laço
+  if( i % 2 == 0) { //condição
+    console.log(`O valor de i, no laço WHILE é: ${i}`)
+  }
+  i++              //incremento
+}
 
+console.log('============ LAÇO, LOOP, DO-WHILE ==================')
+//do-while
+let passo = 0                      //inicialização da variavel
+do {                               // faça 
+  console.log(`Passo ${passo}`)    // bloco de lógica
+  passo++                          // incremento
+} while(passo < 10)                 // enquanto
 
+console.log('============ LAÇO, LOOP, FOR-IN ==================')
 
 
