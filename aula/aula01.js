628a974a56a47837f91b1f6f8d970c1e3df1de4a
Flavio Silva <fal_silva@hotmail.com>
2023-06-15 22:22:41 -0300

    Condicionais JS: If e Else

--- aula/aula01.js
+++ aula/aula01.js
@@ -210,3 +210,20 @@ console.log(`Segunda cor da letra a: ${a}`)
 console.log(`Segunda cor da letra b: ${b}`)
 
 console.log('============ Condicionais do JS ==================')
+//Condicional IF e ELSE
+//Horas do dia
+let horaAtual = 19                //Forma Manual
+let data = new Date()             //Data Atual
+//let horaAtual = data.getHours() //Pegando a hora da data atual
+
+if (horaAtual > 6 && horaAtual < 12){
+  console.log("Bom dia!")
+} else if(horaAtual > 12 && horaAtual < 18) {
+  console.log('Boa tarde!') 
+} else if (horaAtual > 18 && horaAtual < 24) {
+  console.log('Boa noite!')
+} else {
+  console.log('Boa Madrugada!')
+}
+
+
