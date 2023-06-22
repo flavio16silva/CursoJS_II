6e8e10a272b793388db8588e9d3200e2e8c68a45
Flavio Silva <fal_silva@hotmail.com>
2023-06-21 21:57:09 -0300

    Exerc03 radar.js

--- Exerc/Exerc03/ex03.js
+++ Exerc/Exerc03/ex03.js
@@ -1,18 +1,29 @@
 console.log ('=========== Calcular Velocidade do Carro ===========')
 
 let velocidade = radar(200)                          //inicialização da variavel
-console.log(velocidade)
+//console.log(velocidade)
 
-function radar(velocidade){                       //passagem do parametro
-  if (velocidade <= 10 && velocidade <= 40){
-      console.log('Abaixo do limite de velocidade')
+function radar(velocidade) {                       //passagem do parametro
+  if (velocidade >= 10 && velocidade <= 40){
+     console.log( `A velocidade de ${velocidade}km esta abaixo do limite.`)
+
   }else if (velocidade > 40 && velocidade <= 60 ){
-    console.log('Dentro do limite de Velocidade')
-  }else if( velocidade > 60 && velocidade <= 80) {
-      console.log('Cuidado, reduzir velocidade')
-  }else if (velocidade > 80 && velocidade < 100) {
-      console.log('Velocidade acima do permitido')
-  }else if (velocidade > 100){
-      console.log('Pare o veículo')
+    console.log(`A velocidade de ${velocidade}km esta dentro do limite.`)
+
+  }else if( velocidade >= 60 && velocidade <= 80) {
+      console.log(`A velocidade de ${velocidade}km esta abaixo do limite.`)
+
+  }else if( velocidade >= 80 && velocidade <= 100) {
+        const pontuacao = Math.floor(((velocidade - 70) / 5))                   //Limite Máximo 70km. A cada 5km, ganha 1 ponto na CNH.
+           if (pontuacao >= 3) {                                                //Maior que 3 pontos, CNH suspensa.
+           console.log( `A velocidade de ${velocidade}km esta acima do limite. Você possui ${pontuacao} pontos na CNH.`)    
+          }   
+
+  }else if (velocidade > 100) {
+        const pontuacao = Math.floor(((velocidade - 70) / 5)) 
+        if (pontuacao >= 12) {                                
+      console.log(`Você estava a ${velocidade}km/h e recebeu ${pontuacao} pontos. CNH Suspensa!`)
+    } 
   }
 }
+
