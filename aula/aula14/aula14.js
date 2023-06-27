70a6c30138af48d8c7511f1a3de511cb70712bb4
Flavio Silva <fal_silva@hotmail.com>
2023-06-27 08:23:51 -0300

    Aula 14 e Aula 15

---
+++ aula/aula14/aula14.js
@@ -0,0 +1,36 @@
+//Funções de Fábrica
+const celular = {
+  marcaCelular: 'ASUS',
+  tamanhoTela: 150,
+  bateria: 5000,
+  cor: 'azul'
+}
+
+//Encapsula essa informação, dentro de um metodo que cria um objeto.
+
+function criarCelular(marcaCelular, tamanhoTela, bateria, cor){
+  return {                      //retornando o objeto
+  //const celular = {
+    marcaCelular, //marcaCelular, //'ASUS',    //deve ser um parametro do metodo, para ficar dinâmico.
+    tamanhoTela,  //tamanhoTela,  // 150,      Quando a "chave:valor" são iguais, basta deixar apenas um.
+    bateria,     //bateria,      //5000,
+    cor,         //cor           //'azul'
+    
+    ligar(){
+      console.log('Fazendo ligação...')
+    }
+  }
+
+  //return celular
+}
+
+//metodo sendo chamado - criarCelular(), com os parametros sendo passados. Assim nos temos um objeto pronto.
+criarCelular('ASUS', 6.5, 5000, 'Azul') 
+
+//Armazenando o metodo acima numa variavel:
+const celularTop = criarCelular('ASUS', 6.5, 5000, 'Azul')
+console.log(celularTop) 
+
+//Otimizando a criação de objetos, havendo como vantagem o encapsulamento de todas as informações num unico metodo.
+ 
+  
---
+++ aula/aula15/aula15.js
@@ -0,0 +1,21 @@
+//Função de Construtor
+
+//Camel Case - umDoisTresQuatro
+function criarCelular(marcaCelular, tamanhoTela, capacidadeBateria, cor){
+  return{
+    marcaCelular,
+    tamanhoTela,
+    capacidadeBateria,
+    cor,
+    ligar(){
+      console.log("Fazendo Ligação...")
+    }
+  }
+}
+
+
+//Criando um objeto atraves desse metodo
+//Pascal Case - UmDoisTresQuatro
+function Celular(){
+
+}
