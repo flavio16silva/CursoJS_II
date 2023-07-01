efd7161eb008b0a35484672dd2e96e3b00252e01
Flavio Silva <fal_silva@hotmail.com>
2023-07-01 15:41:36 -0300

    AULA 16 E 17

---
+++ aula/aula16/aula16.js
@@ -0,0 +1,22 @@
+//Natureza Dinamica de Objetos
+
+//Criando um objeto
+const mouse = {
+  cor: 'verde',
+  marca: 'login',
+  modelo: 'top'
+}
+//Pelo dinamismo do JS se tentarmos "INCLUIR/ADICIONAR" uma nova propriedade como exemplo abaixo, já irá mostrar no console.log.
+mouse.preco = 50 //Exemplo de inclusão de nova propriedade com dinamismo do JS.
+
+//Da mesma forma sobre o objeto acima, podemos INCLUIR/ADICIONAR um metodo conforme exemplo abaixo:
+//Função:
+mouse.trocarPreco = function () {
+  console.log('Mudando o preço do mouse')
+}
+
+//Objetos no JS são totalmente dinâmicos, caso deseje APAGAR/DELETAR uma propriedade ou uma função é possivel.
+//Aqui DELETAMOS a propriedade 'cor', como exemplo.
+delete mouse.cor //Ex.: DELETANDO propriedade
+delete mouse.trocarPreco //Ex.: DELETANDO metodo
+console.log(mouse) //Na impressão vemos que o objeto já encontra-se pronto.
---
+++ aula/aula17/aula17.js
@@ -0,0 +1,38 @@
+//CLONANDO OBJETOS
+//Criar copia de objetos, que já foram criados anteriormente.
+
+//========= 1º FORMA DE CLONAR OBJETO =====================
+//OBJETO
+const celular = {
+  marcaCelular: 'samsung',
+  tamanhoTela: 5.5,
+  cor: 'azul',
+  bateria: 5000,
+  ligar: function () {
+    console.log('Fazendo ligação...')
+  }
+}
+
+//Vai pegar o primeiro parametro 'alvo', para onde que vai esse objeto que iremos copiar.
+//E de onde irá copiar essas informações, de onde irão copiar as propriedades e metodos.
+//celular: objeto sendo passado como 2º parametro
+
+//Armazenando o 'object.assign' em um novo objeto: 'const novoObjeto'
+
+/* ========== Nesse momento testando a impressão do objeto clonado ================
+const novoObjeto = Object.assign({},celular)
+console.log(novoObjeto) //Impressão para ver o novoObjeto
+ */
+
+// ====== Podemos ADICIONAR novas propriedades ao objeto ===============
+const novoObjeto = Object.assign({
+  formato: 'slim'
+},celular)
+console.log(novoObjeto) //Impressão do novoObjeto, com a nova propriedade.
+
+
+//========= 2º FORMA DE CLONAR OBJETO =====================
+//{...NOME_DO_OBJETO} AQUI COLOCAMOS OS 3 PONTOS E O NOME DO OBJETO QUE SERÁ COPIADO, JUNTO COM SUAS PROPRIEDADES E METODOS.
+const objetoDois = {...celular}
+console.log(objetoDois)
+
