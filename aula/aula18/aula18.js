4dd5141fc2c14177d3482793764ddf8bce3b6f69
Flavio Silva <fal_silva@hotmail.com>
2023-07-01 16:12:25 -0300

    AULA 18 E 19

---
+++ aula/aula18/aula18.js
@@ -0,0 +1,21 @@
+//MATH
+//É um objeto embutido que tem propriedades e métodos para constantes e funções matemáticas. Não é um objeto de função.
+// Ao contrário de outros objetos globais, Math não é um construtor.
+// Todas as propriedades e métodos de Math são estáticos.
+// Você pode referenciar a constante PI como Math.PI e você pode chamar a função de seno como Math.sin(x), onde x é o argumento do método.
+// Constantes são definidas com a precisão total de números reais em JavaScript.
+
+//Math.random()
+// A função Math.random() retorna um número pseudo-aleatório no intervalo [0, 1[, ou seja, de 0 (inclusivo) até, mas não incluindo, 1 (exclusivo), que depois você pode dimensionar para um intervalo desejado.
+// A implementação seleciona uma semente para o algoritmo de geração de números aleatórios; esta semente não pode ser escolhida ou reatribuída.
+
+//Basta digitar no console do navegador: Math.random() e observar os números aleatorios sendo retornados.
+
+console.log(Math.random())
+console.log(Math.random())
+
+//A função Math.max() retorna o maior de um ou mais números.
+console.log(Math.max(10, 2, 55, 1))
+
+//A função Math.min() retorna o maior de um ou mais números.
+console.log(Math.min(10, 2, 55, 19))
