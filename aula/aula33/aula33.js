fd186a378e962455298b0e08281119f5c5541a21
Flavio Silva <fal_silva@hotmail.com>
2023-07-20 21:05:49 -0300

    html, css e js

---
+++ aula/aula33/aula33.js
@@ -0,0 +1,24 @@
+//Validador de Convite
+
+function ValidarEntrada(){
+  //value: é o valor que esta dentro do input.
+  let nome = document.getElementById('itexto').value 
+  //let convite = document.getElementById('iconvite').value
+
+  //Lista de convidados incluidos no array
+  ConvidadosEventos = ['Paulo', 'Joao', 'Maria', 'Pedro', 'Sara', 'Claudia']
+
+  if(ConvidadosEventos.includes(nome)){
+    //alert("Preencha o campo Nome!")
+    //return false;
+    document.getElementById('PermissaoDeEntrada').innerText = 'Você tem convite!'
+    }else {
+        document.getElementById('PermissaoDeEntrada').innerText = 'Você não tem convite.'
+      }
+
+}
+
+
+
+
+
