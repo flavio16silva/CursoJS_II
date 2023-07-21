4093a87f4c16155addfcc5d46d93fc477ddeb465
Flavio Silva <fal_silva@hotmail.com>
2023-07-20 21:08:02 -0300

    teste de .value

--- aula/aula33/aula33.js
+++ aula/aula33/aula33.js
@@ -1,24 +1,20 @@
 //Validador de Convite
 
-function ValidarEntrada(){
+function ValidarEntrada() {
   //value: é o valor que esta dentro do input.
-  let nome = document.getElementById('itexto').value 
+  let nome = document.getElementById('itexto').value
   //let convite = document.getElementById('iconvite').value
 
   //Lista de convidados incluidos no array
   ConvidadosEventos = ['Paulo', 'Joao', 'Maria', 'Pedro', 'Sara', 'Claudia']
 
-  if(ConvidadosEventos.includes(nome)){
+  if (ConvidadosEventos.includes(nome)) {
     //alert("Preencha o campo Nome!")
     //return false;
-    document.getElementById('PermissaoDeEntrada').innerText = 'Você tem convite!'
-    }else {
-        document.getElementById('PermissaoDeEntrada').innerText = 'Você não tem convite.'
-      }
-
+    document.getElementById('PermissaoDeEntrada').innerText =
+      'Você tem convite!'
+  } else {
+    document.getElementById('PermissaoDeEntrada').innerText =
+      'Você não tem convite.'
+  }
 }
-
-
-
-
-
